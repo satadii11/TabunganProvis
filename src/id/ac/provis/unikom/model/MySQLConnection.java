cdf5be3b50ae97929bd36bf07434cafe7c15104b
Satria Adi Putra <satadii11@gmail.com>
2018-07-20 16:03:25 +0700

    initial commit

---
+++ src/id/ac/provis/unikom/model/MySQLConnection.java
@@ -0,0 +1,41 @@
+package id.ac.provis.unikom.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * @author satadii11
+ */
+public abstract class MySQLConnection {    
+    
+    private final String URL = "jdbc:mysql://localhost:3306/provis_tabungan";
+    private final String USERNAME = "root";
+    private final String PASSWORD = "root";
+    
+    private Connection conn;
+    
+    public Connection openConnection() {
+        if (conn == null) {
+            try {
+                conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            } catch (SQLException ex) {
+                System.out.println("Error ketika membuka koneksi: " + ex.getMessage());
+            }
+        }
+        
+        return conn;
+    }
+    
+    public void closeConnection() {
+        if (conn != null) {
+            try {
+                conn.close();
+                conn = null;
+            } catch (SQLException ex) {
+                System.out.println("Error ketika menutup koneksi: " + ex.getMessage());
+            }
+        } 
+    }
+    
+}
