74d340fbe5fbabc67f7543f171a5cfd9bfa078a5
Akhsan AF <akhsanandraf7@gmail.com>
2018-07-28 19:42:36 +0700

    Kesan Kesan

--- src/id/ac/provis/unikom/model/BukuTabunganModel.java
+++ src/id/ac/provis/unikom/model/BukuTabunganModel.java
@@ -20,7 +20,7 @@ enum enumSTATUS{
     Aktif  ("Aktif"),
     Ditutup ("Ditutup");
     
-    private String string;
+    private final String string;
     
     private enumSTATUS(String s){
        this.string = s;
@@ -34,12 +34,12 @@ public class BukuTabunganModel extends MySQLConnection{
     private String nomerRekening;
     private String pin;
     private int saldo;
+    private enumSTATUS status;
     private int idNasabah;
     private int saldoTambahan;
     private int SaldoYangDikurangi;
-    private enumSTATUS status;
     
-
+    
     public String getNomerRekening() {
         return nomerRekening;
     }
@@ -55,7 +55,9 @@ public class BukuTabunganModel extends MySQLConnection{
     public void setPin(String pin) {
         this.pin = pin;
     }
-
+    public int getSaldo(){
+        return saldo;
+    }
     public void setSaldo(int saldo) {
         this.saldo = saldo;
     }
@@ -68,10 +70,12 @@ public class BukuTabunganModel extends MySQLConnection{
         this.idNasabah = idNasabah;
     }
     public void increaseSaldo(int saldoTambahan){
-        this.saldoTambahan = saldoTambahan;
+        this.saldo += saldoTambahan;
     }
-    public int decreaseSaldo(int SaldoYangDikurangi){
-        return SaldoYangDikurangi;
+    
+    public void decreaseSaldo(int SaldoYangDikurangi){
+        this.saldo -= SaldoYangDikurangi;
+        
     }
     public String getStatus(){
         return status.getString();
@@ -92,18 +96,16 @@ public class BukuTabunganModel extends MySQLConnection{
     }
     
     public boolean save(){
-        String sql = "INSERT INTO BukuTabungan (idNasabah, nomorRekening, pin, status, saldo, saldoTambahan, SaldoYangDikurangi)"
-                +"VALUES (?, ?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO buku_tabungan (nomer_rekening, pin, saldo, status, id_nasabah)"
+                +"VALUES (?, ?, ?, ?, ?)";
         
             try {
                 PreparedStatement statement = openConnection().prepareStatement(sql);
-                statement.setInt(1, idNasabah);
-                statement.setString(2, nomerRekening);
-                statement.setString(3, pin);
-                statement.setObject(4, status);
-                statement.setInt(5, saldo);
-                statement.setInt(6, saldoTambahan);
-                statement.setInt(7, SaldoYangDikurangi);                 
+                statement.setString(1, nomerRekening);
+                statement.setString(2, pin);
+                statement.setInt(3, saldo);
+                statement.setString(4, status.getString());
+                statement.setInt(5, idNasabah);                
                 statement.execute();              
             } catch (SQLException ex) {
                 JOptionPane.showMessageDialog(null, "Erorr ketika menyimpan : "+ex.getMessage());
@@ -115,18 +117,16 @@ public class BukuTabunganModel extends MySQLConnection{
     }
 
     public boolean update(){
-        String sql = "UPDATE BukuTabungan"
-                +"SET nomorRekening = '?' , pin = '?' , status = '?' , saldo = '?' , saldoTambahan = '?' , SaldoYangDikurangi = '?' "
-                +"WHERE idNasabah = '?'";
+        String sql = "UPDATE buku_tabungan"
+                +"SET nomor_rekening = '?' , pin = '?' , status = '?' , saldo = '?'"
+                +"WHERE id_nasabah = '?'";
             try {
                 PreparedStatement statement = openConnection().prepareStatement(sql);
                 statement.setString(1, nomerRekening);
                 statement.setString(2, pin);
-                statement.setString(3, status);
-                statement.setInt(4, saldo);
-                statement.setInt(5, saldoTambahan);
-                statement.setInt(6, SaldoYangDikurangi);  
-                statement.setInt(7, idNasabah);
+                statement.setString(3, status.getString());
+                statement.setInt(4, saldo);                
+                statement.setInt(5, idNasabah);
                 statement.execute();            
             } catch (SQLException ex) {
                 JOptionPane.showMessageDialog(null, "Erorr ketika mengupdate : "+ex.getMessage());
@@ -137,12 +137,12 @@ public class BukuTabunganModel extends MySQLConnection{
             return true;
     }
     public boolean tutupTabungan(){
-        String sql = "UPDATE BukuTabungan"
+        String sql = "UPDATE buku_tabungan"
                 +"SET  status = '?'"
-                +"WHERE idNasabah = '?'";
+                +"WHERE id_nasabah = '?'";
             try {
                 PreparedStatement statement = openConnection().prepareStatement(sql);
-                statement.setString(1, status);              
+                statement.setString(1, status.getString());              
                 statement.setInt(2, idNasabah);
                 statement.execute();            
             } catch (SQLException ex) {
@@ -155,19 +155,18 @@ public class BukuTabunganModel extends MySQLConnection{
     }
     public ArrayList<BukuTabunganModel> findAllByIdNasabah(){
       ArrayList<BukuTabunganModel> result = new ArrayList<>();
-            String sql = "SELECT * from BukuTabungan";
+            String sql = "SELECT  * FROM buku_tabungan"
+                    +"WHERE id_nasabah= '?'";
         try {
              PreparedStatement statement = openConnection().prepareStatement(sql);
              ResultSet rs = statement.executeQuery();
         while(rs.next()){
-            BukuTabunganModel row = new BukuTabunganModel();
-            row.setIdNasabah(rs.getInt("idNasabah"));
-            row.setNomerRekening(rs.getString("nomerRekening"));
+            BukuTabunganModel row = new BukuTabunganModel();           
+            row.setNomerRekening(rs.getString("nomer_rekening"));
             row.setPin(rs.getString("pin"));
             row.setStatus(rs.getString("status"));
             row.setSaldo(rs.getInt("saldo"));
-            row.setSaldo(rs.getInt("saldoTambahan"));
-            row.setSaldo(rs.getInt("SaldoYangDikurangi"));
+            row.setIdNasabah(rs.getInt("id_nasabah"));
             result.add(row);
         }
         } catch (SQLException ex) {
@@ -178,11 +177,4 @@ public class BukuTabunganModel extends MySQLConnection{
             }   
         return  result;
     }
-       
-    public static void main(String[] args){
-        BukuTabunganModel bukutabungan = new BukuTabunganModel();
-        bukutabungan.setStatus("Aktif");
-        System.out.println(bukutabungan.getStatus());
-    }
 }
- 
